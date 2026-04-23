912e9db0c2443c5fcbe78a70cf8342324707d213
Eun Kho <khoeun@snu.ac.kr>
2026-04-24 00:01:16 +0900

    Enable CORS

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,11 +1,15 @@
 import 'dotenv/config';
 
+import cors from '@fastify/cors';
 import Fastify from 'fastify';
 
 import serverKeyPlugin from './plugins/server-key.js';
 import routes from './routes/index.js';
 
 const app = Fastify({ logger: true });
+await app.register(cors, {
+  origin: true,
+});
 
 await app.register(serverKeyPlugin);
 await app.register(routes);
